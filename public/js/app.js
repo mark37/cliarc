d45482da2dbf1e033367a57dba003629a46b7442
Mark Santos <christian.santos37@gmail.com>
2018-08-30 20:47:01 +0800

    some fix

--- public/js/app.js
+++ public/js/app.js
@@ -14073,9 +14073,9 @@ window.Popper = __webpack_require__(3).default;
  */
 
 try {
-  window.$ = window.jQuery = __webpack_require__(4);
+    window.$ = window.jQuery = __webpack_require__(4);
 
-  __webpack_require__(17);
+    __webpack_require__(17);
 } catch (e) {}
 
 /**
@@ -14097,9 +14097,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 var token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
@@ -14113,10 +14113,10 @@ if (token) {
 window.Pusher = __webpack_require__(38);
 
 window.Echo = new __WEBPACK_IMPORTED_MODULE_0_laravel_echo___default.a({
-  broadcaster: 'pusher',
-  key: "59087228df5316fb414d",
-  cluster: "ap1",
-  encrypted: true
+    broadcaster: 'pusher',
+    key: "59087228df5316fb414d",
+    cluster: "mt1",
+    encrypted: true
 });
 
 /***/ }),
