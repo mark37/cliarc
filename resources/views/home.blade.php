cd93086db7bc264f3a944f18fdb8ba5ee1ef1492
Carl James Pablo <carljamespablo@gmail.com>
2018-09-21 21:16:34 +0800

    CLIARCLD 2018

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,7 +97,7 @@
 
     <footer class="py-5 bg-dark">
         <div class="container">
-          <p class="m-0 text-center text-white">Copyright &copy; CLIARCLD 2018 /p>
+          <p class="m-0 text-center text-white">Copyright &copy; CLIARCLD 2018 </p>
         </div>
     </footer>
 @endsection
