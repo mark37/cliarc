3893cafd491cbd8cfec4277b044a7018081805b6
chinchan37 <christian.santos37@gmail.com>
2018-09-21 08:19:24 +0800

    add about us

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -9,6 +9,30 @@
         <p>2. Produce and maintain quality plant and animal germplasm in the service of LGUs and organized groups.</p>
         <p>3. Cooperate with other R&D institutions in the development, adaptation and commercialization of agri-based production and post-production technologies including the coordination of on-station and on-farm trainings.</p>
       </div>
+
+      <div class="card" style="padding:20px 50px; margin:25px 0">
+        <h4>History</h4>
+        <p>In 1933, the Philippines is under the Commonwealth government headed by Pres.
+          Manuel Luis Quezon is a country to be developed. Since the country’s main livelihood 
+          is agriculture, the government has focused mainly on agricultural programs such as 
+          the upliftment of animal production in the country.</p>
+      </div>
+
+      <div class="card" style="padding:20px 50px; margin:25px 0">
+        <h4>Services</h4>
+        <p>Training and Seminar</p>
+      </div>
+
+      <div class="card" style="padding:20px 50px; margin:25px 0">
+        <h4>Location</h4>
+        <p>LIARC Compound, McArthur Highway, Brgy. Paraiso, Tarlac City, Tarlac
+        2300 Tarlac, Tarlac</p>
+      </div>
+
+      <div class="card" style="padding:20px 50px; margin:25px 0">
+        <h4>Total Area</h4>
+        <p>381,277 sq.m.</p>
+      </div>
     </div>
   </div>
 @endsection
