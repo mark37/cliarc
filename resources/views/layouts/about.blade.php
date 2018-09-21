8b79973c5b64e24207dae273656f318729fc57c1
chinchan37 <christian.santos37@gmail.com>
2018-09-21 08:45:32 +0800

    edit about us

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -2,7 +2,57 @@
 
 @section('content')
   <div class="container">
-    <div class="row justify-content-center  my-5">
+    <div class="about-section paddingTB60 gray-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-sm-6">
+            <div class="about-title clearfix">
+              <h1>About <span>CLIARCMTDC</span></h1>
+              <h3>History</h3>
+              <p class="about-paddingB">In 1933, the Philippines is under the Commonwealth government headed by Pres.
+              Manuel Luis Quezon is a country to be developed. Since the country’s main livelihood 
+              is agriculture, the government has focused mainly on agricultural programs such as 
+              the upliftment of animal production in the country.</p>
+            
+              <h3>Main function</h3>
+              <p class="about-paddingB">1. Conduct adaptation and verification trials of technologies developed by National R&D Centers for appropriate fine-tuning and dissemination.</p>
+              <p>2. Produce and maintain quality plant and animal germplasm in the service of LGUs and organized groups.</p>
+              <p>3. Cooperate with other R&D institutions in the development, adaptation and commercialization of agri-based production and post-production technologies including the coordination of on-station and on-farm trainings.</p>
+
+              <h3>Services</h3>
+              <p class="about-paddingB">Training and Seminar.</p>
+
+              <h3>Location</h3>
+              <p class="about-paddingB">LIARC Compound, McArthur Highway, Brgy. Paraiso, Tarlac City, Tarlac
+              2300 Tarlac, Tarlac</p>
+
+              <h3>Land</h3>
+              <p class="about-paddingB"><strong>Total Area:</strong> 381,277 sq.m. <br /> <strong>Soil Type:</strong> Sandy to Clay</p>
+
+              <h3>Contact Number</h3>
+              <p class="about-paddingB">+63 948 - 316 - 5947</p>
+
+              <div class="about-icons"> 
+                <ul >
+                  <li><a href="https://www.facebook.com/"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a> </li>
+                  <!-- <li><a href="https://twitter.com/"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a> </li> -->
+                  <!-- <li> <a href="https://plus.google.com/"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a> </li> -->
+                  <li> <a href="mailto:test@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a> </li>
+                </ul>       
+              </div>
+            </div>
+          </div>
+        
+          <!-- <div class="col-md-5 col-sm-6">
+            <div class="about-img">
+              <img src="https://devitems.com/preview/appmom/img/mobile/2.png" alt="">
+            </div>
+          </div>	 -->
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="row justify-content-center  my-5">
       <div class="card" style="padding:20px 50px; margin:25px 0">
         <h4>Main function</h4>
         <p>1. Conduct adaptation and verification trials of technologies developed by National R&D Centers for appropriate fine-tuning and dissemination.</p>
@@ -30,9 +80,10 @@
       </div>
 
       <div class="card" style="padding:20px 50px; margin:25px 0">
-        <h4>Total Area</h4>
-        <p>381,277 sq.m.</p>
+        <h4>Land</h4>
+        <p><strong>Total Area: </strong> 381,277 sq.m.</p>
+        <p><strong>Soil Type: </strong> Sandy to Clay</p>
       </div>
-    </div>
+    </div> -->
   </div>
 @endsection
